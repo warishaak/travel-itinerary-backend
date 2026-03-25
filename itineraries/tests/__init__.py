5629fc159ee3fed686881afa08e9cd2a93b10bbd
warisha-k <warisha.kaleem@booking.com>
2026-03-25 23:32:41 +0000

    Add Django's built-in test framework

---
+++ itineraries/tests/__init__.py
@@ -0,0 +1,9 @@
+from .test_api import ItineraryAPITest
+from .test_models import ItineraryModelTest
+from .test_serializers import ItinerarySerializerTest
+
+__all__ = [
+    "ItineraryModelTest",
+    "ItinerarySerializerTest",
+    "ItineraryAPITest",
+]
