d7cd195190265e3c1acefa803096e9130134b721
warisha-k <warisha.kaleem@booking.com>
2026-03-28 18:59:35 +0000

    Show image on homepage

--- itineraries/serializers.py
+++ itineraries/serializers.py
@@ -14,6 +14,7 @@ class ItinerarySerializer(serializers.ModelSerializer):
             "end_date",
             "is_public",
             "activities",
+            "images",
             "created_at",
             "updated_at",
         ]
